042670bcfa404390f8cfc2b3b02af2515aeefcd1
gfalkeenthros <58170723+gfalkeenthros@users.noreply.github.com>
2021-05-04 13:30:02 +0200

    Implemented All CRUD operations in readingsmanager

--- RestService/Managers/ReadingsManager.cs
+++ RestService/Managers/ReadingsManager.cs
@@ -2,13 +2,14 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace RestService.Managers
 {
     public class ReadingsManager
     {
-        public List<Reading> Data { get; }
-        public static List<Reading> TestData = new List<Reading>()
+        private static List<Reading> Data { get; set; }
+        public static List<Reading> TestData { get; set; } = new List<Reading>()
         {
             new Reading(1, 2, DateTime.Today, false, new byte[]{ }),
             new Reading(2, 3, DateTime.Today, true, new byte[]{ })
@@ -16,7 +17,7 @@ namespace RestService.Managers
 
         public ReadingsManager(List<Reading> data)
         {
-            Data = data;
+            Data = data.Where((r) => true).ToList();
         }
 
         public ICollection<Reading> GetAll()
@@ -27,6 +28,36 @@ namespace RestService.Managers
         public Reading GetById(int id)
         {
             return Data.Find((r) => r.ReadingId == id);
+        }
+
+        public void Post(Reading newReading)
+        {
+            Data.Add(newReading);
+        }
+
+        public Reading Update(int id, Reading reading)
+        {
+            int index = Data.FindIndex((e) => e.ReadingId == id);
+            if (index != -1)
+            {
+                Data[index] = reading;
+                return Data[index];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public Reading Delete(int id)
+        {
+            Reading reading = GetById(id);
+            if(reading != null)
+            {
+                Data.Remove(reading);
+            }
+
+            return reading;
         }
     }
 }
--- RestServiceTest/ReadingsManagerTests.cs
+++ RestServiceTest/ReadingsManagerTests.cs
@@ -18,18 +18,65 @@ namespace RestServiceTest
             _manager = new ReadingsManager(ReadingsManager.TestData);
         }
 
+        [TestInitialize]
+        public void MethodInit()
+        {
+            _manager = new ReadingsManager(ReadingsManager.TestData);
+        }
+
         [TestMethod]
         public void Get_Should_ReturnDataList()
         {
-            List<Reading> expected = _manager.Data;
+            List<Reading> expected = ReadingsManager.TestData;
             CollectionAssert.AreEquivalent(expected, (ICollection)_manager.GetAll());
         }
 
         [TestMethod]
-        public void GetById_ShouldReturnReadingsForThatSensor()
+        public void GetById_ShouldReturnAppropriateReading()
         {
-            Reading expected = _manager.Data[0];
+            Reading expected = ReadingsManager.TestData[0];
             Assert.AreEqual(expected, _manager.GetById(2));
+
+        }
+
+        [TestMethod]
+        public void GetById_InvalidId_ShouldReturnNull()
+        {
+            Reading expected = _manager.GetById(-1);
+            Assert.IsNull(expected);
+        }
+
+        [TestMethod]
+        public void Post_Should_UpdateData()
+        {
+            Reading newReading = new Reading(3, 4, DateTime.Now, false, new byte[] { });
+            int originalLength = _manager.GetAll().Count;
+            _manager.Post(newReading);
+            Assert.AreNotEqual(originalLength, _manager.GetAll().Count);
+        }
+
+        [TestMethod]
+        public void Update_ShouldModifyGivenValue()
+        {
+            Reading original = _manager.GetById(2);
+            Reading updated = _manager.Update(2, new Reading(100, 200, DateTime.Now, false, new byte[] { }));
+            Assert.AreNotEqual(original, updated);
+            Assert.AreEqual(updated.SensorId, 100);
+        }
+
+        [TestMethod]
+        public void Update_InvalidId_ShouldReturnNull()
+        {
+            Reading found = _manager.Update(-1, _manager.GetById(2));
+            Assert.IsNull(found);
+        }
+
+        [TestMethod]
+        public void Delete_ShouldRemove_FromData()
+        {
+            int dataLength = _manager.GetAll().Count;
+            _manager.Delete(3);
+            Assert.AreNotEqual(dataLength, _manager.GetAll().Count);
         }
     }
 }
