fc53d72f1599b1aa291c1bef45d4a224d2634e36
gfalkeenthros <58170723+gfalkeenthros@users.noreply.github.com>
2021-05-04 20:24:52 +0200

    Added status codes to readingcontroller

--- RestService/Controllers/ReadingController.cs
+++ RestService/Controllers/ReadingController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using RestService.Managers;
 using RestService.Model;
@@ -15,39 +16,92 @@ namespace RestService.Controllers
     public class ReadingController : ControllerBase
     {
         private static ReadingsManager _manager = new ReadingsManager(ReadingsManager.TestData);
+        private static string _idNotFoundMessage = $"Could not find reading with id ";
         // GET: api/<ReadipngsController>
         [HttpGet]
-        public IEnumerable<Reading> Get()
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<IEnumerable<Reading>> Get()
         {
-            return _manager.GetAll();
+            return Ok(_manager.GetAll());
         }
 
         // GET api/<ReadipngsController>/5
         [HttpGet("{id}")]
-        public Reading Get(int id)
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<Reading> Get(int id)
         {
-            return _manager.GetById(id);
+            Reading reading = _manager.GetById(id);
+            if (reading == null)
+            {
+                return NotFound(_idNotFoundMessage + id);
+            }
+            else
+            {
+                return Ok(_manager.GetById(id));
+            }
         }
 
         // POST api/<ReadipngsController>
         [HttpPost]
-        public Reading Post([FromBody] Reading value)
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<Reading> Post([FromBody] Reading value)
         {
-            return _manager.Post(value);
+            try
+            {
+                Reading reading = _manager.Post(value);
+                string url = Url.RouteUrl(RouteData.Values) + "/" + reading.ReadingId;
+                return Created(url, reading);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         // PUT api/<ReadipngsController>/5
         [HttpPut("{id}")]
-        public Reading Put(int id, [FromBody] Reading value)
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<Reading> Put(int id, [FromBody] Reading value)
         {
-            return _manager.Update(id, value);
+            try
+            {
+
+                Reading reading = _manager.Update(id, value);
+                if (reading == null)
+                {
+                    return NotFound(_idNotFoundMessage + id);
+                }
+                else
+                {
+                    return Ok(reading);
+                }
+            }catch(ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         // DELETE api/<ReadipngsController>/5
         [HttpDelete("{id}")]
-        public Reading Delete(int id)
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        public ActionResult<Reading> Delete(int id)
         {
-            return _manager.Delete(id);
+            Reading reading = _manager.Delete(id);
+            if(reading == null)
+            {
+                return NotFound(_idNotFoundMessage + id);
+            }
+            else
+            {
+                return Ok(reading);
+            }
+
         }
     }
 }
